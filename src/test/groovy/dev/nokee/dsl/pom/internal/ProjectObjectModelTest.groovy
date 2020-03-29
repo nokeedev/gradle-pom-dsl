578da9a4691d3f4a821c2947e8beedc9c9f26e62
Daniel Lacasse <daniel@nokee.dev>
2020-03-28 22:59:02 -0400

    Fix test for non existant pom.xml file

--- src/test/groovy/dev/nokee/dsl/pom/internal/ProjectObjectModelTest.groovy
+++ src/test/groovy/dev/nokee/dsl/pom/internal/ProjectObjectModelTest.groovy
@@ -11,23 +11,11 @@ class ProjectObjectModelTest extends Specification {
 	@Rule
 	final TestNameTestDirectoryProvider temporaryFolder = new TestNameTestDirectoryProvider(getClass())
 
-	protected TestFile getTestDirectory() {
-		return TestFile.of(temporaryFolder.testDirectory)
-	}
-
-	protected TestFile file(Object... path) {
-		return testDirectory.file(path)
-	}
-
-	protected TestFile getPomFile() {
-		return file('pom.xml')
-	}
-
 	def "throws exception when pom.xml file does not exists"() {
 		when:
-		ProjectObjectModel.of(pomFile)
+		ProjectObjectModel.of(new ProjectObjectModelFile(temporaryFolder.testDirectory, 'pom.xml'))
 		then:
 		def exception = thrown(IllegalArgumentException)
-		exception.message == ''
+		exception.message == "pom.xml file doesn't exists."
 	}
 }
